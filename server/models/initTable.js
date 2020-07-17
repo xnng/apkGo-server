caacd8e7b75da9b6b21025b1198159c3b075d7b6
xnng <xnng77@gmail.com>
2020-07-17 19:18:39 +0800

    feat: 添加版本信息查询接口

--- server/models/initTable.js
+++ server/models/initTable.js
@@ -1,9 +1,12 @@
 const AppList = require('./appList')
 const AppVersion = require('./appVersion')
 
-AppList.sync({
-  force: true
-})
-AppVersion.sync({
-  force: true
-})
+// AppList.sync({
+//   force: true
+// })
+// AppVersion.sync({
+//   force: true
+// })
+
+AppList.hasMany(AppVersion, {foreignKey: 'packageName', sourceKey: 'packageName'})
+AppVersion.belongsTo(AppList, {foreignKey: 'packageName', targetKey: 'packageName'})
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,9 +7,22 @@ const AppVersion = require('../models/appVersion')
 
 let queue = []
 
+
 router.get('/getAppList', async (req, res) => {
+  AppList.hasMany(AppVersion, { foreignKey: 'packageName', sourceKey: 'packageName' })
+  AppVersion.belongsTo(AppList, { foreignKey: 'packageName', targetKey: 'packageName' })
   try {
-    const data = await AppList.findAll({ order: [['createdAt', 'DESC']] })
+    const data = await AppList.findAll({
+      order: [['createdAt', 'DESC']],
+      include: [
+        {
+          model: AppVersion,
+          limit: 1,
+          order: [['versionCode', 'DESC']],
+          attributes: ['id', 'versionCode', 'versionName']
+        }
+      ]
+    })
     res.json({ code: 0, data })
   } catch (error) {
     res.json({ code: 1, msg: error.message })
