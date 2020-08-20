0fd2c35a66fff1e1b8bfc304e06357f60bc75fc3
wangx <xnng77@gmail.com>
2020-08-20 12:35:14 +0800

    delete secret

--- src/config/examples.js
+++ src/config/examples.js
@@ -1,7 +1,7 @@
 module.exports = {
   oss: {
-    ossId: 'LTAI4GB9ab2eFNrgNtUS8fdb',
-    ossKey: 'aXrDQVlxQAikegc2mfYYZq26tcqjzp',
+    ossId: '89JLdFwedUS8fdb',
+    ossKey: 'jcvasoxccgmfYYZq26tcqjzp',
     cloudBasePath: 'apk/',
     host: 'https://private-xnng.oss-cn-shanghai.aliyuncs.com',
     callbackUrl: 'http://ali.xnngs.cn:3003/apk/upload/callback',
@@ -12,13 +12,13 @@ module.exports = {
   },
   message: {
     signName: 'apkGo',
-    accessKey: 'LTAI4G7FrU2B5aAfNExptg1S',
-    accessSecret: 'jz5l7ebfL662bszGmqGyeqJ8H9ldMA',
+    accessKey: 'LTAFEFEWWptg1S',
+    accessSecret: 'jz5FWSWWERRGSSDFF9ldMA',
   },
   db: {
     database: 'app_server',
-    username: 'xnng',
-    password: 'xnngscn6653',
+    username: 'xnGGWEGng',
+    password: 'xFF#EGGWE',
     host: 'rm-uf64646la4kpj2469ho.mysql.rds.aliyuncs.com',
     port: 3306,
   },
